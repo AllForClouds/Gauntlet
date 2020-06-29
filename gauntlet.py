275058cb874aa2b6645352338500a6b4b33f704f
WonderInTardis <18202700807@139.com>
2020-06-29 19:09:44 +0800

    debug roll-down

--- gauntlet.py
+++ gauntlet.py
@@ -27,16 +27,15 @@ if user.find('@')>=0:
 
 #点开已有分组
 driver.find_element_by_xpath('//*[@id="app"]/div[1]/div[1]/div[2]/div/div[1]/div[1]/div/ul/li[1]/span[1]').click()
-time.sleep(2)
+time.sleep(1)
 
 #输入组号，如默认中特别关注组号为6
 num=int(input("请输入“音乐剧云次方净化站”所在分组组号num：")) 
 driver.find_element_by_xpath('//*[@id="app"]/div[1]/div[1]/div[2]/div/div[1]/div[2]/ul/li['+str(num)+']/span').click() #特别关注
-time.sleep(5)
+time.sleep(3)
 
 k=int(input("请输入第__条卡黑帖：")) #选择第k条进行卡黑操作
 btn = driver.find_element_by_xpath('//*[@id="app"]/div[1]/div[2]/div[2]/'+'div['+str(k)+']/div/div/footer/div[2]')#查找评论按钮(倒数第二个div[1]可换)
-time.sleep(1)
 btn.click()
 
 time.sleep(2)
@@ -45,26 +44,26 @@ length=len(links)
 print("待处理数量：")
 print(length)
 for i in range(0,length):
-    links=driver.find_elements_by_link_text("网页链接")
-    driver.execute_script('window.scrollBy(0,500)')#向下滚动500
+    driver.execute_script('window.scrollBy(0,'+str(int(500+250*int(i/10)))+')')#向下滚动500
     time.sleep(2)
+    links=driver.find_elements_by_link_text("网页链接")
     links[i].click()
     time.sleep(3)
     tab1=driver.find_element_by_link_text("有害信息")
     tab1.click()
-    time.sleep(2)
+    time.sleep(1)
     tab2=driver.find_element_by_link_text("其他有害信息")
     tab2.click()
     driver.execute_script('window.scrollBy(0,300)')#向下滚动300
-    time.sleep(3)
+    time.sleep(1)
     #点击选框
     check = driver.find_element_by_class_name('inp_chk')
     check.click()
-    time.sleep(2)
+    time.sleep(1)
     submit=driver.find_element_by_link_text("提交")
     submit.click()
     driver.back()
     time.sleep(2)
-    print('\r'+str(int((i*100)/length))+'%', end='')
+    print('\r'+str(int(((i+1)*100)/length))+'%', end='')
 print('DONE')
 
