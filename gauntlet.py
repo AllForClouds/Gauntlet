2335c901929792c6bfa67d8cc33019e6e04bf894
WonderInTardis <1800017871@pku.edu.cn>
2020-07-09 15:05:36 +0800

    format

--- gauntlet.py
+++ gauntlet.py
@@ -6,6 +6,10 @@ from selenium.webdriver.common.action_chains import ActionChains
 weiboUrl='https://passport.weibo.cn/signin/login?entry=mweibo&res=wel&wm=3349&r=https%3A%2F%2Fm.weibo.cn%2F'
 user=input("请输入邮箱/手机号:")
 password=input("请输入密码:")
+num=int(input("请输入“音乐剧云次方净化站”所在分组组号num："))
+k=int(input("请输入第__条卡黑帖：")) 
+if user.find('@')>=0:
+    print("请注意网页验证提示") 
 
 #此处改为chromedriver的本地位置
 driver = webdriver.Chrome(executable_path="/Users/chy/Desktop/chromedriver")
@@ -14,7 +18,8 @@ driver = webdriver.Chrome(executable_path="/Users/chy/Desktop/chromedriver")
 #driver.maximize_window()
 
 driver.get(weiboUrl)
-time.sleep(5)
+st=time.time()
+time.sleep(3)
 driver.find_element_by_id('loginName').clear()
 driver.find_element_by_id('loginName').send_keys(user)
 driver.find_element_by_id('loginPassword').clear()
@@ -22,8 +27,10 @@ driver.find_element_by_id('loginPassword').send_keys(password)
 driver.find_element_by_id('loginAction').click()
 
 #若邮箱登录，需要经过人工验证
-#if user.find('@')>=0:
-    #time.sleep(10)
+if user.find('@')>=0:
+    print("【请在网页进行手动验证】")
+else:
+    print("【用户输入完毕】")
 
 st=time.time()
 while(time.time()-st<10):
@@ -31,20 +38,18 @@ while(time.time()-st<10):
     if len(focus)!=0:
         break
 else:
-    print("---Run Error--")
+    print("---Website Error--")
 focus[0].click()
 time.sleep(1)
-num=int(input("请输入“音乐剧云次方净化站”所在分组组号num：")) 
 driver.find_element_by_xpath('//*[@id="app"]/div[1]/div[1]/div[2]/div/div[1]/div[2]/ul/li['+str(num)+']/span').click() #特别关注
 time.sleep(1)
-k=int(input("请输入第__条卡黑帖：")) 
 btn = driver.find_element_by_xpath('//*[@id="app"]/div[1]/div[2]/div[2]/'+'div['+str(k)+']/div/div/footer/div[2]')#查找评论按钮(倒数第二个div[1]可换)
 btn.click()
 st=time.time()
 while(time.time()-st<10):
     ids=driver.find_elements_by_link_text("音乐剧云次方净化站")
     if len(ids)!=0:
-            break
+        break
 else:
     print("---Identity Error---")
     driver.close()
@@ -61,27 +66,28 @@ for i in range(0,length):
         links=driver.find_elements_by_link_text("网页链接")
         if len(links)!=0:
             break
+    else:
+        print("---Link Error---")
+        driver.close()
+        sys.exit()
     target=links[i]
     driver.execute_script("arguments[0].scrollIntoView();", target)
     driver.execute_script('window.scrollBy(0,-100)')
-    try:
-        links[i].click()
-    except:
-        print("---Link Error---")
+    links[i].click()
     st=time.time()
     while(time.time()-st<10):
         tab1=driver.find_elements_by_link_text("有害信息")
         if len(tab1)!=0:
             break
-    try:
-        tab1[0].click()
-    except:
+    else:
         print("---Type Error---")
         driver.close()
         sys.exit()
+    tab1[0].click()
+    driver.execute_script('window.scrollBy(0,100)')
     tab2=driver.find_element_by_link_text("其他有害信息")
     tab2.click()
-    driver.execute_script('window.scrollBy(0,300)')
+    driver.execute_script('window.scrollBy(0,200)')
     time.sleep(0.5)
     #点击选框
     check = driver.find_element_by_class_name('inp_chk')
